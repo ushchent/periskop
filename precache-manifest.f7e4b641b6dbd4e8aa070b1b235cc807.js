1746addb182cfc8805b6556ed7b9d375e8a437a9
ushchent <ushchent@hello.hello>
2020-02-19 16:48:16 +0300

    Добавить файлы сайта

---
+++ precache-manifest.f7e4b641b6dbd4e8aa070b1b235cc807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e8c087bef777049b1d0cc743beba1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00143161b149d9740d22",
+    "url": "/static/css/main.cd746ede.chunk.css"
+  },
+  {
+    "revision": "bc2853cb6571a5582d71",
+    "url": "/static/js/2.e601aa26.chunk.js"
+  },
+  {
+    "revision": "977b85ae92e0fb68aebdece70c5a2518",
+    "url": "/static/js/2.e601aa26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00143161b149d9740d22",
+    "url": "/static/js/main.ce1fd49d.chunk.js"
+  },
+  {
+    "revision": "2a202b4b0c99ecd2be51",
+    "url": "/static/js/runtime-main.36c03021.js"
+  }
+]);
