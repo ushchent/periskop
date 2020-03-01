8fdde3a7039ce0ad0c1928b803b3b2e5edcf1878
ushchent <ushchent@hello.hello>
2020-03-01 16:40:26 +0300

    Испытания 2-колоночной страницы.

---
+++ precache-manifest.9fd7d8deacae456cd937dd25f510b49d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac987f0af91b6b3a29d3151b85bfe961",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5982f4be1005b0ac0587",
+    "url": "/static/css/main.1891b847.chunk.css"
+  },
+  {
+    "revision": "395004eae991fd95e51c",
+    "url": "/static/js/2.29c61fe9.chunk.js"
+  },
+  {
+    "revision": "977b85ae92e0fb68aebdece70c5a2518",
+    "url": "/static/js/2.29c61fe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5982f4be1005b0ac0587",
+    "url": "/static/js/main.0f28e6c9.chunk.js"
+  },
+  {
+    "revision": "2a202b4b0c99ecd2be51",
+    "url": "/static/js/runtime-main.36c03021.js"
+  }
+]);
